b9db616716d7c632b7723b9c9556482ef0e27d80
Saevel <kamil@dim.pl>
2019-10-16 00:47:19 +0200

    Using RxJava types directly from Spring controllers

--- src/main/java/prv/saevel/daml/java/spring/Application.java
+++ src/main/java/prv/saevel/daml/java/spring/Application.java
@@ -1,22 +1,28 @@
 package prv.saevel.daml.java.spring;
 
+import com.daml.ledger.rxjava.DamlLedgerClient;
+import com.daml.ledger.rxjava.LedgerClient;
+import io.reactivex.Observable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
 
 @SpringBootApplication
 @RestController
 public class Application {
+
+    // DamlLedgerClient ledgerClient;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
     @GetMapping("/")
-    public Mono<String> sample(){
-        
-        return Mono.just("Hello World!");
+    public Observable<String> sample(){
+       return Observable.fromArray("Hello, World!");
     }
 }
